8871ac98de85382acfcf8f37fb487f936fe97dec
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2020-08-27 09:11:46 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ function mapApp() {
         if /* if we're on iOS, open in Apple Maps */ ((navigator.platform.indexOf("iPhone") != -1) ||
             (navigator.platform.indexOf("iPad") != -1) ||
             (navigator.platform.indexOf("iPod") != -1))
-            window.open('maps://maps.google.com/maps/dir//206+Wyoming+Blvd+NE,+Albuquerque,+NM+87123/@35.0766922,-106.5588065,15z/data=!4m9!4m8!1m0!1m5!1m1!1s0x87220a892e96d6b9:0x35fd66f0b7f725f6!2m2!1d-106.5500517!2d35.0766925!3e0');
+            window.open('maps://maps.apple.com/maps/dir//206+Wyoming+Blvd+NE,+Albuquerque,+NM+87123/@35.0766922,-106.5588065,15z/data=!4m9!4m8!1m0!1m5!1m1!1s0x87220a892e96d6b9:0x35fd66f0b7f725f6!2m2!1d-106.5500517!2d35.0766925!3e0');
         else /* else use Google */
             window.open('https://maps.google.com/maps/dir//206+Wyoming+Blvd+NE,+Albuquerque,+NM+87123/@35.0766922,-106.5588065,15z/data=!4m9!4m8!1m0!1m5!1m1!1s0x87220a892e96d6b9:0x35fd66f0b7f725f6!2m2!1d-106.5500517!2d35.0766925!3e0');
     })
