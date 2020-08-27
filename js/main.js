2970e9d8d2a5dc7a0423296e5c9521f768443cdc
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2020-08-26 19:10:35 -0600

    Nav update map update

--- js/main.js
+++ js/main.js
@@ -18,14 +18,33 @@ modal.addEventListener('click', (e) => {
     }
 });
 
-const x = document.querySelector('#close');
-x.addEventListener('click', () => {
-    const nav = document.querySelector('.nav');
-    nav.style.display = 'none';
-});
+function openModal() {
 
-const open = document.querySelector('#open');
-open.addEventListener('click', () => {
-    const nav = document.querySelector('.nav');
-    nav.style.display = 'block';
-});
+    let open = document.querySelector('#open');
+    open.addEventListener('click', () => {
+        let navMdl = document.querySelector('nav');
+        navMdl.classList.add('move');
+
+    });
+
+    let x = document.querySelector('#close');
+    x.addEventListener('click', () => {
+        let navMdl = document.querySelector('nav');
+        navMdl.classList.remove('move');
+    });
+};
+openModal();
+
+function mapApp() {
+    let maps = document.querySelector('.maps');
+    maps.addEventListener('click', () => {
+        if /* if we're on iOS, open in Apple Maps */ ((navigator.platform.indexOf("iPhone") != -1) ||
+            (navigator.platform.indexOf("iPad") != -1) ||
+            (navigator.platform.indexOf("iPod") != -1))
+            window.open("https://maps.apple.com/maps/place/206+Wyoming+Blvd+NE,+Albuquerque,+NM+87123/@35.0766925,-106.5522404,17z/data=!4m5!3m4!1s0x87220a892e96d6b9:0x35fd66f0b7f725f6!8m2!3d35.0766925!4d-106.5500517");
+        else /* else use Google */
+            window.open("https://maps.google.com/maps/place/206+Wyoming+Blvd+NE,+Albuquerque,+NM+87123/@35.0766925,-106.5522404,17z/data=!4m5!3m4!1s0x87220a892e96d6b9:0x35fd66f0b7f725f6!8m2!3d35.0766925!4d-106.5500517");
+    })
+
+};
+mapApp();
