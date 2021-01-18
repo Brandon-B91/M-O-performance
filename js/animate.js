b0d0ae55bfbec449a3b10821c478adfb1081b369
Brandon-B91 <43705011+Brandon-B91@users.noreply.github.com>
2021-01-18 15:53:01 -0700

    update to GSAP
    changed animation times in animate.js file for the serivces and gallery sections.

--- js/animate.js
+++ js/animate.js
@@ -1,53 +1,92 @@
-// gsap.to(".left", {
-//     scrollTrigger:
-//      ".left", 
-//      x: -100, x: 0, duration: .75,
-//      opacity: 1,
-//      stagger: {
-//          amount: .05
-//      }
-// });
 
-// gsap.to(".right", {
-//     scrollTrigger:
-//      ".right", 
-//      x: 0, duration: 1,
-//      opacity: 1,
-//      stagger: {
-//          grid: [1, 4],
-//          amount: .05
-//      }
-// });
 
-ScrollTrigger.batch(".left", {
-  onEnter: batch => 
-  gsap.to(batch, 
-    {
-        x:0, 
-        opacity:1, 
-        stagger: {each: 1, 
-            overwrite: true}
-    })    
-})
 
-ScrollTrigger.batch(".right", {
-    onEnter: batch => 
-    gsap.to(batch, 
-      {
-          x:0, 
-          opacity:1, 
-          stagger: {each: 1, 
-            overwrite: true}
-        })
-})
+gsap.utils.toArray(".left").forEach(box => {
+    var tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: box,
+        // toggleActions: "play none none none",
+        start: "top 90%",
+        markers: false
+      }
+    });
+  
+    tl.to(box, {
+      duration: .7,
+      opacity: 1,
+      x: 0
+    });
+  });
 
-ScrollTrigger.batch(".gallery-img", {
-    onEnter: batch => 
-    gsap.to(batch, 
-      {
-          y:0, 
-          opacity:1, 
-          stagger: {each: 1, 
-            overwrite: true}
-        })
-  })
+  gsap.utils.toArray(".right").forEach(box => {
+    var tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: box,
+        // toggleActions: "play none none none",
+        start: "top 90%",
+        markers: false
+      }
+    });
+  
+    tl.to(box, {
+      duration: .7,
+      opacity: 1,
+      x: 0
+    });
+  });
+
+  gsap.utils.toArray(".gallery-img").forEach(box => {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: box,
+        // toggleActions: "play none none none",
+        start: "top 80%",
+        markers: false
+      }
+    });
+  
+    tl.to(box, {
+      duration: 1,
+      opacity: 1,
+      y: 0
+    });
+  });
+
+
+// ScrollTrigger.batch(".left", {
+//   onEnter: batch => 
+//   gsap.to(batch, 
+//     {
+//         x:0, 
+//         opacity:1, 
+//         stagger: {each: 2, 
+//             overwrite: true,
+//             duration: 1
+//         },
+//     })    
+// })
+
+
+// ScrollTrigger.batch(".right", {
+//     onEnter: batch => 
+//     gsap.to(batch, 
+//       {
+//           x:0, 
+//           opacity:1, 
+//           stagger: {each: 1, 
+//             overwrite: true},
+//         })
+// })
+
+// ScrollTrigger.batch(".gallery-img", {
+//     onEnter: batch => 
+//     gsap.to(batch, 
+//       {
+//           y:0, 
+//           opacity:1, 
+//           stagger: {each: 1, 
+//             overwrite: true}
+//         })
+//   })
+
+
